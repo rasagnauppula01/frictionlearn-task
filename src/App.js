4ac51d0232ad9ab10f252fb65be87f7009e089d6
rasagnauppula01 <rasagnauppula10@gmail.com>
2024-06-14 17:03:01 +0530

    fln

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './Componentss/Header';
+import HeroSection from './Componentss/HeroSection';
+import Services from './Componentss/Services';
+import CompaniesTrust from './Componentss/CompaniesTrust';
+import Clients from './Componentss/Clients';
+import Footer from './Componentss/Footer';
 import './App.css';
+import DevelopmentSteps from './Componentss/DevelopmentSteps';
+import CaseStudies from './Componentss/CaseStudies';
+
+import PeopleWork from './Componentss/PeopleCarousal';
+import SoftwareCS from './Componentss/SoftwareBuildings';
+import DesignDev from './Componentss/DesignApproach';
+import TechStacks from './Componentss/TechStack';
+import FeaturedResources from './Componentss/FeaturedResources';
+import HireBanner from './Componentss/HireBanner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HeroSection />
+      <Services />
+      <CompaniesTrust />
+      <PeopleWork/>
+      <CaseStudies/>
+      <SoftwareCS/>
+      <DesignDev/>
+      <TechStacks/>
+      <DevelopmentSteps/>
+      <FeaturedResources/>
+      <HireBanner/>
+      <Footer />
+      
     </div>
   );
 }
